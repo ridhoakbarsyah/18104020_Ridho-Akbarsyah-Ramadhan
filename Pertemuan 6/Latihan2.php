f635f17bb34becd24122e0852e45e6d7642ba898
RidhoAja <ridhoakbarsyah15@gmail.com>
2020-11-14 12:01:13 +0700

    Pertemuan 6

---
+++ Pertemuan 6/Latihan2.php
@@ -0,0 +1,30 @@
+<?php
+	$a = "Hello"; // tipe data String
+	$b = 20; // tipe data integer
+	$c = 3.14; // tipe data float
+	$d = true; // tipe data boolean
+
+	echo $a;
+	echo "<br>";
+	echo $b;
+	echo "<br>";
+	echo $c;
+	echo "<br>";
+	echo $d;
+	echo "<br>";
+
+	$nama = "Ridho";
+	echo "Nama ".$nama;
+	print "<br> <h1 style='background-color: lightblue;'>Ashiap Mantap !!</h1>";
+	echo "Hasil = " . ($b + $c);
+	echo "<br>";
+
+	var_dump($a);
+	echo "<br>";
+	var_dump($b);
+	echo "<br>";
+	var_dump($c);
+	echo "<br>";
+	var_dump($d);
+
+?>
